965125bff09427fa21a983e827e2991a398fb1b9
DaXterGD <faniel5431@gmail.com>
2023-11-19 19:36:06 -0500

    creación del modelo de productos

---
+++ server/products/products.dao.mjs
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+import schema from './products.schema.mjs'
+
+// se importa el esquema de la base de datos para definir métodos para guardar datos y obtener los datos
+schema.static = {
+  create: (data, cb) => {
+    const product = new this(data)
+    product.save(cb)
+  },
+  get: (query, cb) => {
+    this.find(query, cb)
+  }
+}
+
+// una vez que se tiene el esquema y métodos de la base de datos definidos, se crea el modelo
+const ProductsModel = mongoose.model('products', schema)
+export default ProductsModel
---
+++ server/products/products.schema.mjs
@@ -0,0 +1,31 @@
+import { Schema } from 'mongoose'
+
+// definición del esquema de la base de datos con sus tipos y restricciones
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+export default schema
